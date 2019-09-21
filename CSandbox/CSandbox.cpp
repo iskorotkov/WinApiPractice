58c373b453444e78b7299e850e2c657e3139d0c7
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-09-21 09:10:48 +0500

    Use WNDCASSEX

--- CSandbox/CSandbox.cpp
+++ CSandbox/CSandbox.cpp
@@ -1,39 +1,70 @@
 #include <Windows.h>
 #include <tchar.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-const wchar_t* clName = _T("MyClassName");
-const wchar_t* clWinName = _T("MyWindowName");
+const TCHAR czWinClass[] = _T("MyClassName");
+const TCHAR czWinName[] = _T("MyWindowName");
+HWND hwnd;
+HBRUSH hBrush;
 
-LRESULT CALLBACK WinProc(HWND wnd, UINT msg, WPARAM wParam, LPARAM lParam)
+void RunNotepad(void)
 {
-	switch (msg)
-	{
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
-	}
-	return DefWindowProc(wnd, msg, wParam, lParam);
+	STARTUPINFO sInfo;
+	PROCESS_INFORMATION pInfo;
+
+	ZeroMemory(&sInfo, sizeof(STARTUPINFO));
+
+	puts("Starting Notepad...");
+	CreateProcess(_T("C:\\Windows\\Notepad.exe"), NULL, NULL, NULL, FALSE, 0, NULL, NULL, &sInfo, &pInfo);
 }
 
-int main()
+LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    HINSTANCE hThisInstance = GetModuleHandle(NULL);
-	WNDCLASS wincl = { 0 };
-	wincl.hInstance = hThisInstance;
-	wincl.lpszClassName = clName;
-	wincl.lpfnWndProc = WinProc;
-
-	if (!RegisterClass(&wincl))
+	switch (message)
 	{
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+	}
+	return DefWindowProc(hwnd, message, wParam, lParam);
+}
+
+int main(int argc, char** argv)
+{
+	BOOL bMessageOk;
+	MSG message;
+
+	WNDCLASSEX wincl = { 0 };
+
+	int nCmdShow = SW_SHOW;
+
+	HINSTANCE hThisInstance = GetModuleHandle(NULL);
+
+	wincl.cbSize = sizeof(wincl);
+	wincl.style = CS_HREDRAW | CS_VREDRAW;
+	wincl.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wincl.hCursor = LoadCursor(NULL, IDC_ARROW);
+	
+	wincl.hInstance = hThisInstance;
+	wincl.lpszClassName = czWinClass;
+	wincl.lpfnWndProc = WindowProcedure;
+
+	hBrush = CreateSolidBrush(RGB(255,255,255));
+	wincl.hbrBackground = hBrush;
+
+	if (!RegisterClassEx(&wincl))
+	{
+		MessageBox(NULL, L"Class registration has failed!", L"Error", MB_OK | MB_ICONERROR);
 		DWORD error = GetLastError();
-		printf("Error: %ul\n", error);
+		printf("Error: %lu\n", error);
 		return error;
 	}
 
-	HWND wnd = CreateWindow(
-		clName,
-		clWinName,
+	hwnd = CreateWindowEx(
+		0,
+		czWinClass,
+		czWinName,
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT,
 		CW_USEDEFAULT,
@@ -42,28 +73,28 @@ int main()
 		HWND_DESKTOP,
 		NULL,
 		hThisInstance,
-		NULL,
+		NULL
 	);
 
-	ShowWindow(wnd, SW_SHOW);
-
-	BOOL bOk = FALSE;
-	MSG msg;
+	ShowWindow(hwnd, nCmdShow);
 
 	// PeekMessage - async
-	while ((bOk = GetMessage(&msg, NULL, 0, 0)) != 0)
+	while ((bMessageOk = GetMessage(&message, NULL, 0, 0)) != 0)
 	{
-		if (bOk < 0)
+		if (bMessageOk < 0)
 		{
-			// Process error
+			puts("Suddenly, GetMessage failed! You can call GetLastError() to see what happened");
+            break;
 		}
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		TranslateMessage(&message);
+		DispatchMessage(&message);
 	}
 
-	DestroyWindow(wnd);
+	DestroyWindow(hwnd);
+	UnregisterClass(czWinClass, hThisInstance);
+	DeleteObject(hBrush);
 
-	UnregisterClass(clName, hThisInstance);
+	RunNotepad();
 
-	return msg.lParam;
+	return 0;
 }
