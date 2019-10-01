945446c3514a224670d78fea94943ecfda83969c
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-10-01 19:36:44 +0500

    Allow to write preferences without annoying zeros

--- WinApiPractice/files.h
+++ WinApiPractice/files.h
@@ -14,16 +14,12 @@ enum class ReadingMethod
 
 const TCHAR* configFile = _T("C:\\Projects\\WinApiPractice\\WinApiPractice\\config.ini");
 
-const UINT BUFFSIZE = 1024;
-const UINT FILE_MAP_START = 0;
-
-HANDLE hMapFile;     // handle for the file's memory-mapped region
-HANDLE hFile;        // the file handle
-LPVOID lpMapAddress; // pointer to the base address of the memory-mapped region
-
 Preferences* ReadConfigFromFileMapping()
 {
-	hFile = CreateFile(configFile,
+	const UINT BUFFSIZE = 1024;
+	const UINT FILE_MAP_START = 0;
+
+	HANDLE hFile = CreateFile(configFile,
 		GENERIC_READ | GENERIC_WRITE,
 		0,
 		NULL,
@@ -62,7 +58,7 @@ Preferences* ReadConfigFromFileMapping()
 	DWORD dwFileSize = GetFileSize(hFile, NULL);
 	_tprintf(TEXT("hFile size: %10d\n"), dwFileSize);
 
-	hMapFile = CreateFileMapping(hFile,
+	HANDLE hMapFile = CreateFileMapping(hFile,
 		NULL,
 		PAGE_READWRITE,
 		0,
@@ -74,7 +70,7 @@ Preferences* ReadConfigFromFileMapping()
 		return NULL;
 	}
 
-	lpMapAddress = MapViewOfFile(hMapFile,
+	LPVOID lpMapAddress = MapViewOfFile(hMapFile,
 		FILE_MAP_ALL_ACCESS,
 		0,
 		dwFileMapStart,
@@ -95,16 +91,88 @@ Preferences* ReadConfigFromFileMapping()
 	{
 		// Failed to read from the view.
 	}
+
+	UnmapViewOfFile(lpMapAddress);
+	CloseHandle(hMapFile);
+	CloseHandle(hFile);
+
 	return prefs;
 }
 
 void WriteConfigToFileMapping(Preferences* prefs)
 {
-	if (prefs == NULL || lpMapAddress == NULL)
+	if (prefs == NULL)
 	{
-		_tprintf(L"Preferences are NULL or file mapping is not created.");
+		_tprintf(L"Preferences are NULL.");
+		return;
 	}
+
 	const TCHAR* content = PreferencesToString(prefs);
+
+	const UINT BUFFSIZE = wcslen(content) * sizeof TCHAR;
+	const UINT FILE_MAP_START = 0;
+
+	HANDLE hFile = CreateFile(configFile,
+		GENERIC_READ | GENERIC_WRITE,
+		0,
+		NULL,
+		CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
+
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		_tprintf(TEXT("hFile is NULL\n"));
+		_tprintf(TEXT("Target file is %s\n"),
+			configFile);
+		return;
+	}
+
+	SYSTEM_INFO SysInfo; // system information; used to get granularity
+	GetSystemInfo(&SysInfo);
+	DWORD dwSysGran = SysInfo.dwAllocationGranularity;
+
+	DWORD dwFileMapStart = (FILE_MAP_START / dwSysGran) * dwSysGran;
+	_tprintf(TEXT("The file map view starts at %ld bytes into the file.\n"),
+		dwFileMapStart);
+
+	DWORD dwMapViewSize = (FILE_MAP_START % dwSysGran) + BUFFSIZE;
+	_tprintf(TEXT("The file map view is %ld bytes large.\n"),
+		dwMapViewSize);
+
+	DWORD dwFileMapSize = FILE_MAP_START + BUFFSIZE;
+	_tprintf(TEXT("The file mapping object is %ld bytes large.\n"),
+		dwFileMapSize);
+
+	int iViewDelta = FILE_MAP_START - dwFileMapStart;
+	_tprintf(TEXT("The data is %d bytes into the view.\n"),
+		iViewDelta);
+
+	DWORD dwFileSize = GetFileSize(hFile, NULL);
+	_tprintf(TEXT("hFile size: %10d\n"), dwFileSize);
+
+	HANDLE hMapFile = CreateFileMapping(hFile,
+		NULL,
+		PAGE_READWRITE,
+		0,
+		dwFileMapSize,
+		NULL);
+	if (hMapFile == NULL)
+	{
+		_tprintf(TEXT("hMapFile is NULL: last error: %d\n"), GetLastError());
+		return;
+	}
+
+	LPVOID lpMapAddress = MapViewOfFile(hMapFile,
+		FILE_MAP_ALL_ACCESS,
+		0,
+		dwFileMapStart,
+		dwMapViewSize);
+	if (lpMapAddress == NULL)
+	{
+		_tprintf(TEXT("lpMapAddress is NULL: last error: %d\n"), GetLastError());
+		return;
+	}
 
 	wcscpy_s((TCHAR*)lpMapAddress, BUFFSIZE, content);
 
