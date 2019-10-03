41db6c7a0ea35b33467468b951fa8b612dbcf40b
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-10-03 08:24:39 +0500

    Fix streams not writing to the config file

--- WinApiPractice/filestream.cpp
+++ WinApiPractice/filestream.cpp
@@ -1,5 +1,3 @@
-#define _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING
-
 #include "filestream.h"
 #include <fstream>
 #include "preferences.h"
@@ -14,7 +12,7 @@ Preferences* ReadConfigUsingStream(const TCHAR* configFile)
 	const UINT size = ifs.tellg();
 	ifs.seekg(0, std::ios::beg);
 	const UINT bufferSize = (size % 2 == 1 ? size + 1 : size) / sizeof(wchar_t);
-	
+
 	TCHAR* buffer = new TCHAR[bufferSize + 1];
 	buffer[bufferSize] = L'\0';
 
@@ -27,10 +25,8 @@ void WriteConfigUsingStream(const TCHAR* configFile, const Preferences* prefs)
 {
 	const TCHAR* content = PreferencesToString(prefs);
 
-	std::wofstream ofs;
-	ofs << content;
+	std::ofstream ofs(configFile, std::ios::binary);
+	ofs.write((char*)content, wcslen(content) * sizeof(wchar_t));
 
 	delete content;
 }
-
-#undef _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING
