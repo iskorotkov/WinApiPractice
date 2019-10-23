f984b40cfee4e8e4d4b7262aa220562c0225033d
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-10-23 20:11:42 +0500

    Add file with conversions between different number of channels

---
+++ libpic/Conversions.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include "Conversions.h"
+#include <exception>
+#include <string>
+
+// ReSharper disable once CppInconsistentNaming
+void FromRGB::ToRGBA(const std::byte* source, std::byte* dest)
+{
+	dest[0] = source[2];
+	dest[1] = source[1];
+	dest[2] = source[0];
+	dest[4] = std::byte(0);
+}
+
+// ReSharper disable once CppInconsistentNaming
+void FromRGB::ToRGB(const std::byte* source, std::byte* dest)
+{
+	dest[0] = source[2];
+	dest[1] = source[1];
+	dest[2] = source[0];
+}
+
+void Change::ChannelsNumber(const std::byte* source, std::byte* dest, const int sourceChannels, const int destChannels)
+{
+	if (sourceChannels == 3)
+	{
+		if (destChannels == 4)
+		{
+			FromRGB::ToRGBA(source, dest);
+			return;
+		}
+	}
+	else if (sourceChannels == 4)
+	{
+		if (destChannels == 4)
+		{
+			FromRGB::ToRGB(source, dest);
+			return;
+		}
+	}
+	throw std::exception("Can't change channels number.");
+}
---
+++ libpic/Conversions.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <cstddef>
+
+// ReSharper disable once CppInconsistentNaming
+namespace FromRGB
+{
+	// ReSharper disable once CppInconsistentNaming
+	void ToRGBA(const std::byte* source, std::byte* dest);
+	
+	// ReSharper disable once CppInconsistentNaming
+	void ToRGB(const std::byte* source, std::byte* dest);
+};
+
+namespace Change
+{
+	void ChannelsNumber(const std::byte* source, std::byte* dest, int sourceChannels, int destChannels);
+}
