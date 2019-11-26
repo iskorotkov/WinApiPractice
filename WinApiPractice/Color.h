254b71deddcb314018c0792e420bbbaf799800d4
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-11-26 18:08:13 +0500

    Add Color and DynamicColor classes

---
+++ WinApiPractice/Color.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include <Windows.h>
+
+constexpr int BLACK = 0;
+constexpr int WHITE = 255;
+
+struct Color
+{
+	Color() = default;
+	Color(int red, int green, int blue);
+	
+	int red = BLACK;
+	int green = BLACK;
+	int blue = BLACK;
+
+	static Color Red();
+	static Color Green();
+	static Color Blue();
+
+	[[nodiscard]] COLORREF AsColorRef() const;
+	
+	Color operator+(const Color& other) const;
+	Color operator*(float multiplier) const;
+};
