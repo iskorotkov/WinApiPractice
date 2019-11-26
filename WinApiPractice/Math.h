b33a6bdb925b6becdc45181e1876ee620e38e918
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-11-26 18:08:41 +0500

    Add Math class with Clamp and Interpolate methods

---
+++ WinApiPractice/Math.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+namespace Math
+{
+	template <typename T, typename U>
+	T Clamp(T value, U min, U max)
+	{
+		if (value <= min)
+		{
+			return min;
+		}
+		if (value >= max)
+		{
+			return max;
+		}
+		return value;
+	}
+
+	template <typename T>
+	T Interpolate(T from, T to, float value)
+	{
+		value = Clamp(value, 0.0f, 1.0f);
+		return from * (1 - value) + to * value;
+	}
+
+	int Floor(const float value);
+}
